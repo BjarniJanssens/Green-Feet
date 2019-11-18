ddccb05b3d364dd39d426e27aafe30abce17600c
Bjarni Janssens <bjarni.janssens@student.ehb.be>
2019-11-18 09:28:57 +0100

    Add all different page components

---
+++ Webapplicatie/src/app/trophys/trophys.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trophys',
+  templateUrl: './trophys.component.html',
+  styleUrls: ['./trophys.component.css']
+})
+export class TrophysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
