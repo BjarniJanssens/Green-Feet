48fb728cd06f34c7d43d781307d88e09c27f066e
Bjarni Janssens <bjarni.janssens@student.ehb.be>
2019-11-18 09:27:36 +0100

    Add app-component

---
+++ Webapplicatie/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { DevicesComponent } from './devices/devices.component';
+import { TrophysComponent } from './trophys/trophys.component';
+import { CompareComponent } from './compare/compare.component';
+import { AccountComponent } from './account/account.component';
+import { SettingsComponent } from './settings/settings.component';
+import { PowerUsageComponent } from './shared/power-usage/power-usage.component';
+import { PowerChartComponent } from './shared/power-chart/power-chart.component';
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'rooms', component: RoomsComponent},
+  {path: 'devices', component: DevicesComponent},
+  {path: 'trophys', component: TrophysComponent},
+  {path: 'compare', component: CompareComponent},
+  {path: 'account', component: AccountComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: 'powerCircle', component: PowerUsageComponent},
+  {path: 'powerChart', component: PowerChartComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Webapplicatie/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import {ChartsModule} from 'ng2-charts';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './main/nav-bar/nav-bar.component';
+import { HeaderComponent } from './main/header/header.component';
+import { PowerUsageComponent } from './shared/power-usage/power-usage.component';
+import { HomeComponent } from './home/home.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { DevicesComponent } from './devices/devices.component';
+import { TrophysComponent } from './trophys/trophys.component';
+import { CompareComponent } from './compare/compare.component';
+import { AccountComponent } from './account/account.component';
+import { SettingsComponent } from './settings/settings.component';
+import { PowerChartComponent } from './shared/power-chart/power-chart.component';
+import { CalendarComponent } from './shared/calendar/calendar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HeaderComponent,
+    PowerUsageComponent,
+    HomeComponent,
+    RoomsComponent,
+    DevicesComponent,
+    TrophysComponent,
+    CompareComponent,
+    AccountComponent,
+    SettingsComponent,
+    PowerChartComponent,
+    CalendarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
+    ChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
