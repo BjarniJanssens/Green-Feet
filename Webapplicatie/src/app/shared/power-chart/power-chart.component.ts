2bb8d97e7cf342fc26eb264f12258aeb957eeae7
Bjarni Janssens <bjarni.janssens@student.ehb.be>
2019-11-18 15:24:22 +0100

    Line-chart aangemaakt van de power usage

--- Webapplicatie/src/app/shared/power-chart/power-chart.component.ts
+++ Webapplicatie/src/app/shared/power-chart/power-chart.component.ts
@@ -1,46 +1,93 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartDataSets } from 'chart.js';
-import { Color, Label } from 'ng2-charts';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
+import {
+  ChartDataSets
+} from 'chart.js';
+import {
+  Color,
+  Label
+} from 'ng2-charts';
 
 @Component({
   selector: 'app-power-chart',
   templateUrl: './power-chart.component.html',
   styleUrls: ['./power-chart.component.css']
 })
+
 export class PowerChartComponent implements OnInit {
+  @ViewChild('myCanvas', {
+    static: true
+  }) canvas: ElementRef;
   public chartData: ChartDataSets[] = [{
-    data: [65, 59, 80, 81, 56, 80],
-    label: 'KW Generated'
+    data: [3.9, 4.2, 3.7, 4.9, 2.3, 3.3, 4.2, 2.5, 3.6, ],
+    label: ''
   }];
-  public chartLabels: Label[] = ['4:00', '8:00', '12:00', '16:00', '20:00', '24:00'];
+  public chartLabels: Label[] = ['5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12AM'];
   public chartOptions = {
+    type: 'line',
     responsive: true,
+    options: {
+      title: {
+        display: true,
+      },
+    },
     scales: {
       yAxes: [{
         ticks: {
           beginAtZero: true,
-          max: 100
+          fontColor: '#f8f8ff',
+          fontFamily: 'displayBold',
+          max: 10
+        },
+        scaleLabel: {
+          display: true,
+          fontColor: '#f8f8ff',
+          fontFamily: 'displayBold',
+          labelString: 'kWh'
+        },
+        gridLines: {
+          display: true,
+          color: '#f8f8ff'
+        },
+      }],
+      xAxes: [{
+        ticks: {
+          fontColor: '#f8f8ff',
+          fontFamily: 'displayBold',
         }
-      }]
+      }],
     },
     legend: {
-      display: false
+      display: false,
     },
-    title: {
-      display: true,
-      text: 'KW Generated today'
-    }
   };
+
   public chartLegend = true;
   public chartType = 'line';
 
   public chartColors: Color[] = [{
-    backgroundColor: 'rgb(0, 214, 198, 0.5)',
-    borderColor: 'rgb(0, 214, 198)'
   }];
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const gradient = this.canvas.nativeElement.getContext('2d').createLinearGradient(0, 0, 0, 280);
+    gradient.addColorStop(1, '#4694FF');
+    gradient.addColorStop(.9, '#4694FF');
+    gradient.addColorStop(.6, '#41F2DE');
+    gradient.addColorStop(0, '#41F2DE');
+    this.chartColors = [{
+      borderColor: '#4694FF',
+      borderWidth: 5,
+      pointBackgroundColor: '#f8f8ff',
+      pointBorderColor: '#f8f8ff',
+      pointBorderWidth: 1,
+      backgroundColor: gradient
+    }];
+  }
 
 }
